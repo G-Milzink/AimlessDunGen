133cae40973be46d50aa3bbff742c5a32d872088
Gustaaf Milzink <107410648+G-Milzink@users.noreply.github.com>
2023-12-17 15:37:00 +0100

    everything stable

--- ADG/Camera.gd
+++ ADG/Camera.gd
@@ -6,6 +6,5 @@ func _ready():
 
 func _process(delta):
 	if get_tree().get_first_node_in_group("player"):
-		self.set_zoom(lerp(self.get_zoom(), zoom_target, 5*delta))
+		self.set_zoom(lerp(self.get_zoom(), zoom_target, 2.5*delta))
 		position = get_tree().get_first_node_in_group("player").position
-
