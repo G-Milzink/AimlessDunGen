d17b5e684e9c17896e9a1fdbbf4d9f9af8e46fb0
Gustaaf Milzink <107410648+G-Milzink@users.noreply.github.com>
2023-12-28 12:36:56 +0100

    fix minor bug where rng light particles would always start emitting.

--- lighting/light_rng.gd
+++ lighting/light_rng.gd
@@ -3,10 +3,12 @@ extends Node2D
 var energy_deviation: float
 
 @onready var light = $PointLight2D
+@onready var cpu_particles_2d = $StaticBody2D/CPUParticles2D
 
 func _ready():
 	if randi() % 100 >= 50:
 		light.set_enabled(false)
+		cpu_particles_2d.emitting = false
 
 func _process(delta):
 	if light:
